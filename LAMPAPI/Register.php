48695ed91a018a47e20fdd01b94fd2abadcab0b6
Mekha0 <mehreenk767@gmail.com>
2026-01-27 00:45:13 -0500

    Nahum's Register.php file

---
+++ LAMPAPI/Register.php
@@ -0,0 +1,130 @@
+
+<?php
+    $env = parse_ini_file('.env');
+
+    $db_host = getenv('DB_SERVER');
+	$db_user = getenv('DB_USER');
+	$db_password = getenv('DB_PASS');
+	$db_name = getenv('DB_NAME');
+
+    $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+    if ($conn->connect_error) {
+
+        returnWithError($conn->connect_error);
+
+    }
+
+    else {
+
+        $inData = getRequestInfo();
+
+        $firstName = $inData["firstName"];
+
+        $lastName = $inData["lastName"];
+
+        $login = $inData["login"];
+
+        $password = $inData["password"];
+
+
+
+        if (empty($firstName)) {
+
+            returnWithError("Please enter a firstname.");
+
+        }
+
+        elseif (empty($lastName)) {
+
+            returnWithError("Please enter a lastname.");
+
+        }
+
+        elseif (empty($login)) {
+
+            returnWithError("Please enter a login (username).");
+
+        }
+
+        elseif (empty($password)) {
+
+            returnWithError("Please enter a password.");
+
+        }
+
+        else {
+
+            $stmt = $conn->prepare(
+
+            "INSERT INTO Users (firstName, lastName, login, password)
+
+             VALUES (?,?,?,?)");
+
+
+
+            $stmt->bind_param("ssss",$firstName,$lastName,$login,$password);
+
+            $stmt->execute();
+
+            $stmt->close();
+
+
+
+            returnWithInfo( $firstName, $lastName, $login);
+
+        }
+
+        $conn->close();
+
+    }
+
+
+
+    function getRequestInfo()
+
+    {
+
+        return json_decode(file_get_contents('php://input'), true);
+
+    }
+
+
+
+    function sendResultInfoAsJson( $obj )
+
+    {
+
+        header('Content-type: application/json');
+
+        echo $obj;
+
+    }
+
+
+
+    function returnWithInfo( $firstName, $lastName, $login)
+
+	{
+
+		$retValue = '{"firstName":"' . $firstName . '","lastName":"' . $lastName . '","login":"' . $login . '","error":""}';
+
+		sendResultInfoAsJson( $retValue );
+
+	}
+
+
+
+    function returnWithError( $err )
+
+    {
+
+        $retValue = '{"error":"' . $err . '"}';
+
+        sendResultInfoAsJson( $retValue );
+
+    }
+
+
+
+?>
