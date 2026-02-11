a327583a67f447af3f2f23b86b464cc2fa1c5cbd
Nahum Auguste <151590695+Nahum-Auguste@users.noreply.github.com>
2026-02-11 15:01:10 -0500

    Completed the Edit Contact File as it was missing the ability to edit first and last names.

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -8,15 +8,30 @@
 	$env = parse_ini_file('.env');
 	$first_name = $inData["firstName"];
 	$last_name = $inData["lastName"];
-    if (array_key_exists("phone",$inData)) {
-        $phone = $inData["phone"];
+
+    if (array_key_exists("newFirstName",$inData)) {
+        $newFirstName = $inData["newFirstName"];
+    }
+    else {
+        $newFirstName = null;
+    }
+
+    if (array_key_exists("newLastName",$inData)) {
+        $newLastName = $inData["newLastName"];
+    }
+    else {
+        $newLastName = null;
+    }
+
+    if (array_key_exists("newPhone",$inData)) {
+        $phone = $inData["newPhone"];
     }
     else {
         $phone = null;
     }
     
-    if (array_key_exists("email",$inData)) {
-        $email = $inData["email"];
+    if (array_key_exists("newEmail",$inData)) {
+        $email = $inData["newEmail"];
     }
     else {
         $email = null;
@@ -31,8 +46,8 @@
 	$db_name = $env['DB_NAME'];
   
 
-    if (empty($first_name) || empty($last_name) || (empty($phone)&&empty($email)) || empty($userId)) {
-        returnWithError("Missing 1 or more appropriate request fields. Looking for userId, firstname, lastname, phone, and email.");
+    if (empty($first_name) || empty($last_name) || (empty($phone)&&empty($email)&&empty($newFirstName)&&empty($newLastName)) || empty($userId)) {
+        returnWithError("Missing 1 or more appropriate request fields. Looking for userId, firstname, lastname, and at least one of these values: newFirstName, newLastName, newPhone, or newEmail.");
     }
 
 	$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
@@ -57,6 +72,30 @@
         
         $editedPhone = false;
         $editedEmail = false;
+        $editedFirstName = false;
+        $editedLastName = false;
+
+        //Update User Contact First Name
+        if (!empty($newFirstName)) {
+            $stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE UserID=? AND FirstName=? AND LastName=?;");
+            $stmt->bind_param("ssss", $newFirstName,$userId,$first_name, $last_name);
+            $stmt->execute();
+            if ($stmt->error) {
+                returnWithError($stmt->error);
+            }
+            $editedFirstName = true;
+        }
+
+        //Update User Contact First Name
+        if (!empty($newLastName)) {
+            $stmt = $conn->prepare("UPDATE Contacts SET LastName=? WHERE UserID=? AND FirstName=? AND LastName=?;");
+            $stmt->bind_param("ssss", $newLastName,$userId,$first_name, $last_name);
+            $stmt->execute();
+            if ($stmt->error) {
+                returnWithError($stmt->error);
+            }
+            $editedLastName = true;
+        }
         
         //Update User Contact Phone Number
         if (!empty($phone)) {
@@ -81,17 +120,22 @@
 		    returnWithError($stmt->error);
         }
         else {
-            if ($editedEmail==true || $editedPhone==true) {
-                if ($editedEmail==false) {
-                    returnWithInfo("Successfully edited the phone number.");
-                }
-                if ($editedPhone==false) {
-                    returnWithInfo("Successfully edited the email.");
-                }
-                if ($editedEmail && $editedPhone) {
-                    returnWithInfo("Successfully edited the phone number and email.");
-                }
+            $info = "";
+
+            if ($editedPhone) {
+                $info .= "Successfully edited the phone number.\n";
             }
+            if ($editedEmail) {
+                $info .= "Successfully edited the email.\n";
+            }
+            if ($editedFirstName) {
+                $info .= "Successfully edited the firstname.\n";
+            }
+            if ($editedLastName) {
+                $info .= "Successfully edited the lastname.\n";
+            }
+
+            returnWithInfo($info);
         }
 
         $stmt->close();
